aafb06779b0599679ddc8377682e4a6d50bfb5e3
eric jubera <ericjubera@gmail.com>
2022-10-03 16:22:49 -0500

    controls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,25 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/plants" do
+    plants = Plant.all
+    plants.to_json
+end 
+
+get "/rooms" do
+  rooms=Room.all 
+  rooms.to_json
+
+end 
+
+post "/rooms/new" do
+  rooms=Room.create(name: params[:name])
+  rooms.to_json
+end
+
+post "/plants/new" do
+plants=Plant.create(name: params[:name], image: params[:image], care_level: params[:care_level], size: params[:size])
+plants.to_json
+end 
+
 end
